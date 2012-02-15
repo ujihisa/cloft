6faea55f3287e67bba4ce774220e86b88c5cf5d1
ujihisa <ujihisa@gmail.com>
2012-02-14 17:50:18 -0800

    hmm...

--- src/cloft/core.clj
+++ src/cloft/core.clj
@@ -34,16 +34,6 @@
          get-entity-death-listener get-entity-explode-listener zombieze
          get-entity-damage-listener get-entity-projectile-hit-listener)
 
-; originally from clojure-contrib
-(defmacro with-ns
-  "Evaluates body in another namespace.  ns is either a namespace
-  object or a symbol.  This makes it possible to define functions in
-  namespaces other than the current one."
-  [ns & body]
-  `(binding [*ns* (the-ns ~ns)]
-     ~@(map (fn [form] `(eval '~form)) body)))
-
-
 (defn- lingr [msg]
   (future-call
     #(clj-http.client/post
@@ -54,9 +44,18 @@
          :text (str msg)
          :bot_verifier "[FIXME]"}})))
 
+; originally from clojure-contrib
+(defmacro with-ns
+  "Evaluates body in another namespace.  ns is either a namespace
+  object or a symbol.  This makes it possible to define functions in
+  namespaces other than the current one."
+  [ns & body]
+  `(binding [*ns* (the-ns ~ns)]
+     ~@(map (fn [form] `(eval '~form)) body)))
 
 (defn enable-plugin [plugin]
-    (eval (macroexpand (list 'with-ns 'cloft.core (read-string (slurp "dynamic.clj")))))
+    (eval (macroexpand
+            (list 'with-ns 'cloft.core (read-string (str "(do " (slurp "dynamic.clj") ")")))))
     (def plugin* plugin)
     (def server* (.getServer plugin*))
     (def plugin-manager* (.getPluginManager server* ))
