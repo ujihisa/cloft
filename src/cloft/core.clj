df0ee3d0d2d1b7f3de30b6ef9ca3f645673633be
ujihisa <ujihisa@gmail.com>
2012-02-15 17:30:21 -0800

    helpers

--- src/cloft/core.clj
+++ src/cloft/core.clj
@@ -83,10 +83,11 @@
 (def world (Bukkit/getWorld "world"))
 (def place1 (org.bukkit.Location. world -55.5 71.5 73.5)) ; lighter
 (def place2 (org.bukkit.Location. world -63.5 71.5 73.5)) ; darker
-(def place3 (org.bukkit.Location. world 18.46875 111.0 41.53125 -10.501099 1.5000023)) ; on top of tree
+(def place3 (org.bukkit.Location. world 18.46875 103.0 41.53125 -10.501099 1.5000023)) ; on top of tree
 (def place4 (org.bukkit.Location. world -363.4252856675041 65.0 19.551327467732065 -273.89978 15.149968)) ; goboh villae
 (def place5 (org.bukkit.Location. world -5 73 -42.5)) ; top of pyramid
 (def place6 (org.bukkit.Location. world 308.98823982676504 78 133.16713120198153 -55.351166 20.250006)) ; dessert village
+(defn ujm (Bukkit/getPlayer "ujm"))
 (defn get-player-move []
   (c/auto-proxy
     [org.bukkit.event.player.PlayerListener] []
