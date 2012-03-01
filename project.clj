d93a07b760e187d8a8ea6be2869659c93fbccab7
sbwhitecap <shigemichik@gmail.com>
2012-03-02 03:08:58 +0900

    use bukkit 1.1-R6

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[clj-http "0.3.1"]
                  [org.clojure/core.match "0.2.0-alpha9"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.1-R4"]
+  :dev-dependencies [[org.bukkit/bukkit "1.1-R6"]
                      [clj-minecraft "1.0.0-SNAPSHOT"]
                      [org.clojure/clojure "1.3.0"]
                      [org.clojure/tools.logging "0.2.3"]]
