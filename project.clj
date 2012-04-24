a4379a4949869c3780e592083857feea012d45a1
ujihisa <ujihisa@gmail.com>
2012-04-24 15:38:02 -0700

    removing obsolete dependency

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[clj-http "0.3.1"]
                  [swank-clojure/swank-clojure "1.3.3"]
                  [org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.2.5-R1.2-SNAPSHOT"]
-                     [clj-minecraft "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.2.5-R1.2-SNAPSHOT"]]
   ;:repl-options [:init nil :caught clj-stacktrace.repl/pst+]
   ;:repositories {"spout-repo-snap" "http://repo.getspout.org/content/repositories/snapshots/"
   ;               "spout-repo-rel" "http://repo.getspout.org/content/repositories/releases/"})
