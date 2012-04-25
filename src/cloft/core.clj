099d130eec9eec6937ef92488b7f12f88d05c2e8
ujihisa <ujihisa@gmail.com>
2012-04-24 21:58:03 -0700

    closures a mob for 20 sec

--- src/cloft/core.clj
+++ src/cloft/core.clj
@@ -732,6 +732,32 @@
           (c/consume-itemstack (.getInventory shooter) Material/WEB))
         (= arrow-skill-explosion (get @jobs (.getDisplayName shooter)))
         (.damage target 10 shooter)
+        (= arrow-skill-ice (get @jobs (.getDisplayName shooter)))
+        (when (not (.isDead target))
+          (let [loc (.getLocation (.getBlock (.getLocation target)))]
+            (doseq [y [0 1]]
+              (doseq [[x z] [[-1 0] [1 0] [0 -1] [0 1]]]
+                (let [block (.getBlock (.add (.clone loc) x y z))]
+                  (when (#{Material/AIR Material/SNOW} (.getType block))
+                    (.setType block Material/GLASS)))))
+            (doseq [y [-1 2]]
+              (let [block (.getBlock (.add (.clone loc) 0 y 0))]
+                (when (#{Material/AIR Material/SNOW} (.getType block))
+                  (.setType block Material/GLASS))))
+            (future-call #(do
+                            (Thread/sleep 1000)
+                            (.teleport target (.add (.clone loc) 0.5 0.0 0.5))))
+            (future-call #(do
+                            (Thread/sleep 20000)
+                            (doseq [y [0 1]]
+                              (doseq [[x z] [[-1 0] [1 0] [0 -1] [0 1]]]
+                                (let [block (.getBlock (.add (.clone loc) x y z))]
+                                  (when (= (.getType block) Material/GLASS)
+                                    (.setType block Material/AIR)))))
+                            (doseq [y [-1 2]]
+                              (let [block (.getBlock (.add (.clone loc) 0 y 0))]
+                                (when (= (.getType block) Material/GLASS)
+                                  (.setType block Material/AIR))))))))
         (= 'fly (get @jobs (.getDisplayName shooter)))
         (future-call #(c/add-velocity target 0 1 0))
         (= 'mobchange (get @jobs (.getDisplayName shooter)))
