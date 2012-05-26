0e67945217b3aad59f1b83324a8be280295dc26b
ujihisa <ujihisa@gmail.com>
2012-05-26 16:45:29 -0700

    bukkit version up

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[clj-http "0.3.1"]
                  [swank-clojure/swank-clojure "1.3.3"]
                  [org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.2.5-R1.3-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.2.5-R3.1-SNAPSHOT"]]
   ;:repl-options [:init nil :caught clj-stacktrace.repl/pst+]
   ;:repositories {"spout-repo-snap" "http://repo.getspout.org/content/repositories/snapshots/"
   ;               "spout-repo-rel" "http://repo.getspout.org/content/repositories/releases/"})
