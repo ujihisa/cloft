37031dc6fb38c456f95a49ea094f5976849756c2
ujihisa <ujihisa@gmail.com>
2012-07-29 16:22:35 -0700

    127.0.0.1 is for some reason 0:0:0:0:0:0:0:1

--- src/cloft/core.clj
+++ src/cloft/core.clj
@@ -960,7 +960,7 @@
                     (let [ip (.. player getAddress getAddress getHostAddress)]
                       (if (or
                             (= "10.0" (apply str (take 4 ip)))
-                            (= "127.0.0.1" ip)
+                            #_(= "127.0.0.1" ip)
                             (= "113.151.154.229" ip)
                             (= "0:0:0:0:0:0:0:1" ip))
                         (do
