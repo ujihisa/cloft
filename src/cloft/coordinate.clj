21a24d92d54b1270a2563ffda1b161a153c9c302
ujihisa <ujihisa@gmail.com>
2012-08-04 19:02:24 -0700

    refactoring: generalizing an internal util func

--- src/cloft/coordinate.clj
+++ src/cloft/coordinate.clj
@@ -4,14 +4,13 @@
 (defn xz-normalized-vector [v]
   (.normalize (Vector. (.getX v) 0.0 (.getZ v))))
 
-(defn player-direction [player]
-  (let [loc (.getLocation player)
-        height (Vector. 0.0 1.0 0.0)
+(defn direction-of [loc]
+  (let [height (Vector. 0.0 1.0 0.0)
         depth (xz-normalized-vector (.getDirection loc))
         right-hand (.crossProduct (.clone depth) height)]
    [depth height right-hand]))
 
 (defn local-to-world [player origin-block dx hx rx]
-  (let [[d h r] (player-direction player)
+  (let [[d h r] (direction-of (.getLocation player))
         loc (.toVector (.getLocation origin-block))]
     (.add loc (.add (.add (.multiply d dx) (.multiply h hx)) (.multiply r rx)))))
