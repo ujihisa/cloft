a21235ea41f87f9c344150968b187bc67bd5a6a3
ujihisa <ujihisa@gmail.com>
2012-08-31 22:50:10 -0700

    trivial refactoring. direction-of and xz-normalized-vector are private

--- src/cloft/coordinate.clj
+++ src/cloft/coordinate.clj
@@ -1,17 +1,15 @@
 (ns cloft.coordinate
   (:import [org.bukkit.util Vector]))
 
-(defn xz-normalized-vector [v]
-  (.normalize (Vector. (.getX v) 0.0 (.getZ v))))
-
-(defn direction-of [loc]
-  (let [height (Vector. 0.0 1.0 0.0)
-        depth (xz-normalized-vector (.getDirection loc))
-        right-hand (.crossProduct (.clone depth) height)]
-   [depth height right-hand]))
-
 (defn local-to-world [player origin-block dx hx rx]
   """returns a bukkit Vector"""
+  (defn direction-of [loc]
+    (let [height (Vector. 0.0 1.0 0.0)
+          xz-normalized-vector #(.normalize (Vector. (.getX %) 0.0 (.getZ %)))
+          depth (xz-normalized-vector (.getDirection loc))
+          right-hand (.crossProduct (.clone depth) height)]
+     [depth height right-hand]))
+
   (let [[d h r] (direction-of (.getLocation player))]
     (.add (.toVector (.getLocation origin-block))
           (.add (.add (.multiply d dx) (.multiply h hx)) (.multiply r rx)))))
