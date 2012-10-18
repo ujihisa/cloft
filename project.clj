7026e3f57d23f1bee21afa32eb30794f628ba8a8
ujihisa <ujihisa@gmail.com>
2012-10-17 23:40:45 -0700

    library version up

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cloft "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[clj-http "0.5.5"]
-                 [swank-clojure/swank-clojure "1.3.3"]
+  :dependencies [[clj-http "0.5.6"]
+                 [swank-clojure/swank-clojure "1.4.2"]
                  [org.zmq/jzmq "1.0.0"]
                  [org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[org.bukkit/bukkit "1.3.2-R0.3-SNAPSHOT"]]
