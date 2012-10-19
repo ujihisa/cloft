6c98b058bef7854e6e8f2d1e8523f8fabe31dc49
ujihisa <ujihisa@gmail.com>
2012-10-18 21:03:49 -0700

    bukkit version up

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [swank-clojure/swank-clojure "1.4.2"]
                  [org.zmq/jzmq "1.0.0"]
                  [org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.3.2-R0.3-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.3.2-R2.1-SNAPSHOT"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
   :javac-options {:destdir "classes/"}
