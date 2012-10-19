01048e04f1cf85a96ad0746ae44c7e093f603ea2
ujihisa <ujihisa@gmail.com>
2012-10-18 21:10:36 -0700

    indentation

--- src/cloft/chimera_cow.clj
+++ src/cloft/chimera_cow.clj
@@ -82,18 +82,18 @@
                       (future
                         (Thread/sleep 800)
                         (later
-                        (.setVelocity c (Vector. 0.0 0.5 0.0))
-                        (let [dire (.subtract (.clone (.getLocation player))
-                                              (.clone (.getLocation c)))
-                              vect (.normalize (.toVector dire))]
-                          (doseq [_ [0 1 2]]
-                            (let [fb (.launchProjectile c Fireball)]
-                              (.setShooter fb c)
-                              (.setYield fb 0.0)
-                              (.teleport fb (.add (.clone (.getLocation c)) vect))
-                              (Thread/sleep 300)
-                              (.setDirection fb vect)
-                              (.setVelocity fb (.add vect (Vector. (- (rand) 0.5) 0.0 (- (rand) 0.5))))))))))
+                          (.setVelocity c (Vector. 0.0 0.5 0.0))
+                          (let [dire (.subtract (.clone (.getLocation player))
+                                                (.clone (.getLocation c)))
+                                vect (.normalize (.toVector dire))]
+                            (doseq [_ [0 1 2]]
+                              (let [fb (.launchProjectile c Fireball)]
+                                (.setShooter fb c)
+                                (.setYield fb 0.0)
+                                (.teleport fb (.add (.clone (.getLocation c)) vect))
+                                (Thread/sleep 300)
+                                (.setDirection fb vect)
+                                (.setVelocity fb (.add vect (Vector. (- (rand) 0.5) 0.0 (- (rand) 0.5))))))))))
                     (letfn [(rand1 []
                               (* 0.8 (- (rand) 0.5)))]
                       (dotimes [_ 50]
