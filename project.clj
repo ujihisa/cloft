f48959722604d62c3c526b49cf7f12510794384a
ujihisa <ujihisa@gmail.com>
2012-11-24 18:09:28 -0800

    WitherSkeleton: 100exp (temporary)

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :dependencies [[clj-http "0.5.6"]
                  [swank-clojure/swank-clojure "1.4.2"]
                  [org.zmq/jzmq "1.0.0"]
-                 [org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.4.5-R0.1-SNAPSHOT"]]
+                 [org.clojure/clojure "1.4.0"][org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
   :javac-options {:destdir "classes/"}
