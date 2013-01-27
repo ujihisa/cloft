017eab47c414e3d552a3832ba086aac352af4afe
ujihisa <ujihisa@gmail.com>
2013-01-26 16:40:48 -0800

    version up

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject cloft "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[clj-http "0.5.6"]
-                 [swank-clojure/swank-clojure "1.4.2"]
+                 [swank-clojure/swank-clojure "1.5.0-SNAPSHOT"]
                  [org.zmq/jzmq "1.0.0"]
-                 [org.clojure/clojure "1.4.0"][org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.4.5-R0.3-SNAPSHOT"]]
+                 [org.clojure/clojure "1.4.0"]
+                 [org.bukkit/bukkit "1.4.7-R0.2-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.4.7-R0.2-SNAPSHOT"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
   :javac-options {:destdir "classes/"}
