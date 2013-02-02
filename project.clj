cee0c21be729d6eaba05d88d3efa92663ec05acd
ujihisa <ujihisa@gmail.com>
2013-02-02 09:51:20 -0800

    bukkit up

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :dependencies [[clj-http "0.5.6"]
                  [swank-clojure/swank-clojure "1.5.0-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]
-                 [org.bukkit/bukkit "1.4.7-R0.2-SNAPSHOT"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.4.7-R0.2-SNAPSHOT"]
+                 [org.bukkit/bukkit "1.4.7-R1.1-SNAPSHOT"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.4.7-R1.1-SNAPSHOT"]
                      [org.zmq/jzmq "1.0.0"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
