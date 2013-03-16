9e967149085620248e488133e3c2a2b5d88ddb3c
ujihisa <ujihisa@gmail.com>
2013-03-16 00:19:29 -0700

    1.5

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :dependencies [[clj-http "0.6.4"]
                  [swank-clojure/swank-clojure "1.5.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.0"]
-                 [org.bukkit/bukkit "1.4.7-R1.1-SNAPSHOT"]
-                 [org.dynmap/dynmap-api "1.6"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.4.7-R1.1-SNAPSHOT"]
+                 [org.bukkit/bukkit "1.5-R0.1-SNAPSHOT"]
+                 [org.dynmap/dynmap-api "1.5"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.5-R0.1-SNAPSHOT"]
                      [org.zeromq/jeromq "0.2.0"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
