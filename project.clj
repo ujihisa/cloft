7b5f1dacae806e579294b3eb54dab6274d39c34e
ujihisa <ujihisa@gmail.com>
2013-07-15 19:35:15 -0700

    update all bukkit-related versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject cloft "1.0.0-SNAPSHOT"
+(defproject cloft "1.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :license {:name "GPL-3 or later"}
   :dependencies [[clj-http "0.6.4"]
                  [swank-clojure/swank-clojure "1.5.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  [org.bukkit/bukkit "1.5-R0.1-SNAPSHOT"]
-                 [org.dynmap/dynmap-api "1.5"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.5-R0.1-SNAPSHOT"]
+                 [org.dynmap/dynmap-api "1.8"]]
+  :dev-dependencies [[org.bukkit/bukkit "1.6.2-R0.1-SNAPSHOT"]
                      [org.zeromq/jeromq "0.2.0"]]
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
